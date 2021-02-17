58a68527bc229c7aebbc3f96382a9314ba17b611
zhuang002 <zhuang001@gmail.com>
2021-02-16 19:59:29 -0500

    using-hashmap

--- TreeDepthSample/src/Main.java
+++ TreeDepthSample/src/Main.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
-	static Node tree;
-	static Node[] nodeArray=null;
+	static HashMap<Integer,ArrayList<Integer>> tree=new HashMap<>();
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ReadInput();
-		System.out.println(treeDepth(nodeArray[1]));
+		System.out.println(1);
 		// System.out.println(treeDepth(root));
 
 	}
 
-	private static int treeDepth(Node parent) {
+	private static int treeDepth(int parent) {
 		// TODO Auto-generated method stub
-		
-		if (parent.children.isEmpty()) {
+		ArrayList<Integer> children=tree.get(parent);
+		if (children.isEmpty()) {
 			return 0;
 		}
 		int maxDepth=Integer.MIN_VALUE;
-		for (Node subNode:parent.children) {
+		for (int subNode:children) {
 			int depth=treeDepth(subNode);
 			if (maxDepth<depth) {
 				maxDepth=depth;
@@ -38,38 +38,20 @@ public class Main {
 		Scanner sc=new Scanner(System.in);
 		int lines=sc.nextInt();
 		int nodes=sc.nextInt();
-		nodeArray=new Node[nodes+1];
-		for (int i=0;i<nodeArray.length;i++) {
-			nodeArray[i]=null;
-		}
+		
 		
 		for (int i=0;i<lines;i++) {
 			int id1=sc.nextInt();
 			int id2=sc.nextInt();
-			Node n1,n2;
-			if (nodeArray[id1]==null) {
-				n1=new Node();
-				n1.id=id1;
-				nodeArray[id1]=n1;
-			} else {
-				n1=nodeArray[id1];
-			}
-			if (nodeArray[id2]==null) {
-				n2=new Node();
-				n2.id=id2;
-				nodeArray[id2]=n2;
-			} else {
-				n2=nodeArray[id2];
-			}
-			n1.children.add(n2);	
+			if (!tree.containsKey(id1)) {
+				tree.put(id1,new ArrayList<Integer>());
+			} 
+			ArrayList<Integer> children=tree.get(id1);
+			children.add(id2);
+			
 		}
 		
 	}
 
 }
 
-
-class Node {
-	int id;
-	ArrayList<Node> children=new ArrayList<>();
-}
